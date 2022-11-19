620bac0ce55609d2b9fe4ca6ddc3c3171ca1bda2
vinlin <vincas.linkevicius@ktu.edu>
2022-11-19 17:57:28 +0200

    doing work

--- DTO/Rating/RatingDtoGet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ModHub.DTO;
-
-public class RatingDtoGet
-{
-    public int Id { get; set; }
-    public int ModId { get; set; }
-    public int Rating { get; set; }
-    public int UserId { get; set; }
-}
--- DTO/Rating/RatingDtoPut.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ModHub.DTO;
-
-public class RatingDtoPut
-{
-    [Range(1, 5)]
-    public int Rating { get; set; }
-}
